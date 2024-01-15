68bf5ad77108f5082615d3f18766617c07f91cd4
TestingTroupDortmund <156596792+TestingTroupDortmund@users.noreply.github.com>
2024-01-15 14:33:29 +0100

    Update PizzaOrder.java
    Methode Platzhalter TODO eingefügt

--- PizzaKonfigurator/src/main/java/PizzaOrder.java
+++ PizzaKonfigurator/src/main/java/PizzaOrder.java
@@ -13,79 +13,8 @@ public class PizzaOrder implements InvoiceSupport{
 
     @Override
     public double getAmount() throws Exception {
-            double totalAmount = 0;
-
-            // Teigpreise
-            switch (pizzaConfiguration.getDough()) {
-                case WHITE:
-                    totalAmount += 2.0;
-                    break;
-                case WHOLE_GRAIN_30:
-                    totalAmount += 2.5;
-                    break;
-                case WHOLE_GRAIN_80:
-                    totalAmount += 3.0;
-                    break;
-                case GLUTEN_FREE:
-                    totalAmount += 3.5;
-                    break;
-            }
-
-            // Soßenpreise
-            switch (pizzaConfiguration.getSauce()) {
-                case TOMATO:
-                    totalAmount += 1.0;
-                    break;
-                case CREME_FRAICHE:
-                    // Zusätzlicher Preis für weiße Soße bei glutenfreiem Teig
-                    if (!(pizzaConfiguration.getDough() == Dough.GLUTEN_FREE)) {
-                        totalAmount += 1.5;
-                    }else{
-                        throw new Exception("Weiße soße gibt es bei glutenfrei nicht!");
-                    }
-                    break;
-                case NONE:
-                    // Keine Soße ausgewählt
-                    break;
-            }
-
-            // Beilagenpreise
-            for (MainTopping topping : pizzaConfiguration.getMainTopping()) {
-                switch (topping) {
-                    case SALAMI:
-                    case MUSHROOM:
-                    case HAM:
-                        totalAmount += 1.0;
-                        break;
-                    case SPINACH:
-                    case ARTICHOKE:
-                        totalAmount += 2.0;
-                        break;
-                    case LACHS:
-                    case DONER:
-                        totalAmount += 3.0;
-                        break;
-                    case GARLIC:
-                        totalAmount += 0.5;
-                        break;
-                }
-            }
-
-            // SubTopping-Preise
-            if (pizzaConfiguration.getSubTopping() != null) {
-                switch (pizzaConfiguration.getSubTopping()) {
-                    case GOUDA:
-                        totalAmount += 1.0;
-                        break;
-                    case MOZZARELLA:
-                        totalAmount += 1.5;
-                        break;
-                    case NONE:
-                        // Kein SubTopping ausgewählt
-                        break;
-                }
-            }
-            return totalAmount;
+            // TODO hier generierte Methode einfügen
+            return 0.0
     }
 
     public void setAmount(double amount){
